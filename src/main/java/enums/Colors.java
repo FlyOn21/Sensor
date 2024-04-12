f1f20a0a3d565cb7cca98a2f9debdd89922a35cc
Zhoholiev Pavlo <zhogolevpv@gmail.com>
2024-04-12 20:35:48 +0300

    fix wrong value Colors.java str 9 (value != null && value == 1.0) to (value != null && value == 0.0) 12.04.24 20:32 Zhogoliev Pavlo

--- src/main/java/enums/Colors.java
+++ src/main/java/enums/Colors.java
@@ -4,9 +4,8 @@ public enum Colors {
     WHITE,
     BLACK;
 
-
     public static Colors fromValue(Double value) {
-        if (value != null && value == 1.0) {
+        if (value != null && value == 0.0) {
             return WHITE;
         } else {
             return BLACK;
